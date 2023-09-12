2f8abc413ced0940ce4d6aef505a0e8458e4e41c
Evansullivan64 <20093137@mail.wit.ie>
2023-09-12 17:11:28 +0100

    Exercise 2.

---
+++ exercise2.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+const todoTitles = []
+     json.forEach( (todo, index) => {
+        todoTitles.push(todo.title)
+       
+    })
+    console.log(todoTitles)
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
